797f60d38208602a0de5e37cc80506350288450e
Musyaitir <musyaitirjr@gmail.com>
2025-09-08 12:57:56 +0900

    project_mobile

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:project_mobile/pages/loginpage.dart';
+
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('flutter Mobile Lanjutan', style: TextStyle(color: Colors.white),),
+          backgroundColor: Colors.blue,
         ),
+        body: const LoginPage(),
       ),
     );
   }
