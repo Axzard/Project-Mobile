4e939f16116da6d212792f1dc5e3a69faff47c30
Musyaitir <musyaitirjr@gmail.com>
2025-09-08 15:53:59 +0900

    progres

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,7 +7,34 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home', style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold)),
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        leading: Icon(Icons.menu, color: Colors.black),
+        title: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+        ),
+          child: TextField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search, color: Colors.black),
+              hint: Text("Cari", style: TextStyle(color: Colors.black,fontSize: 16)),
+              hintStyle: TextStyle(color: Colors.black),
+              contentPadding: EdgeInsets.symmetric(
+                vertical: 5.0,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+          ),
+        ),
+
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(Icons.notifications, color: Colors.black),
+        )],
       ),
 
       body: Center(
